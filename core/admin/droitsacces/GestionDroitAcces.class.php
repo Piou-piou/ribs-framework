1bbef2a09dbf08a3ea39b211b404b6f3fa254ce3
Piou-piou <pilloud.anthony@gmail.com>
2016-03-05 18:31:45 +0100

    update GestionDroitAcces class to simplify it

--- core/admin/droitsacces/GestionDroitAcces.class.php
+++ core/admin/droitsacces/GestionDroitAcces.class.php
@@ -45,30 +45,13 @@
 						$nom_liste[] = $obj->nom_liste;
 
 						//récupération du nombre de droits d'acces pour cette liste
-						$query1 = $dbc->query("SELECT count(ID_droit_acces) as ID_droit_acces FROM liaison_liste_droit WHERE ID_liste_droit_acces =".$obj->ID_liste_droit_acces);
-						if ((is_array($query1)) && (count($query1) > 0)) {
-							foreach ($query1 as $obj1) {
-								$nb_droit_acces[] = $obj1->ID_droit_acces;
-							}
-						}
-
+						$nb_droit_acces = $this->getNombreDroitAccesListe($obj->ID_liste_droit_acces);
 
 						//récupération du nombre d'utilisateurs qui sont dans cette liste
-						$query2 = $dbc->query("SELECT count(ID_identite) as ID_identite FROM identite WHERE liste_droit=".$obj->ID_liste_droit_acces);
-						if ((is_array($query2)) && (count($query2) > 0)) {
-							foreach ($query2 as $obj2) {
-								$nb_user[] = $obj2->ID_identite;
-							}
-						}
-
+						$nb_user = $this->getNombreUtilisateurListe($obj->ID_liste_droit_acces);
 
 						//récupération du nombres de pages dans cette liste
-						$query3 = $dbc->query("SELECT count(ID_page) as ID_page FROM droit_acces_page WHERE ID_liste_droit_acces = $obj->ID_liste_droit_acces");
-						if ((is_array($query3)) && (count($query3) > 0)) {
-							foreach ($query3 as $obj3) {
-								$nb_droit_acces_page[] = $obj3->ID_page;
-							}
-						}
+						$nb_droit_acces_page = $this-> getNombrePageListe($obj->ID_liste_droit_acces);
 					}
 
 					$this->setListeDroitAcces($id_liste_droit_acces, $nom_liste, $nb_droit_acces, $nb_droit_acces_page, $nb_user);
@@ -123,6 +106,63 @@
 		}
 		public function getNbUser() {
 			return $this->nb_user;
+		}
+
+		/**
+		 * @param $id_liste_droit_acces
+		 * @return array
+		 */
+		private function getNombreUtilisateurListe($id_liste_droit_acces) {
+			$dbc = App::getDb();
+
+			$nb_user = 0;
+
+			$query2 = $dbc->query("SELECT count(ID_identite) as ID_identite FROM identite WHERE liste_droit=".$id_liste_droit_acces);
+			if ((is_array($query2)) && (count($query2) > 0)) {
+				foreach ($query2 as $obj2) {
+					$nb_user[] = $obj2->ID_identite;
+				}
+			}
+
+			return $nb_user;
+		}
+
+		/**
+		 * @param $id_liste_droit_acces
+		 * @return array
+		 */
+		private function getNombreDroitAccesListe($id_liste_droit_acces) {
+			$dbc = App::getDb();
+
+			$nb_droit_acces = 0;
+
+			$query1 = $dbc->query("SELECT count(ID_droit_acces) as ID_droit_acces FROM liaison_liste_droit WHERE ID_liste_droit_acces =".$id_liste_droit_acces);
+			if ((is_array($query1)) && (count($query1) > 0)) {
+				foreach ($query1 as $obj1) {
+					$nb_droit_acces[] = $obj1->ID_droit_acces;
+				}
+			}
+
+			return $nb_droit_acces;
+		}
+
+		/**
+		 * @param $id_liste_droit_acces
+		 * @return array|int
+		 */
+		private function getNombrePageListe($id_liste_droit_acces) {
+			$dbc = App::getDb();
+
+			$nb_droit_acces_page = 0;
+
+			$query3 = $dbc->query("SELECT count(ID_page) as ID_page FROM droit_acces_page WHERE ID_liste_droit_acces =".$id_liste_droit_acces);
+			if ((is_array($query3)) && (count($query3) > 0)) {
+				foreach ($query3 as $obj3) {
+					$nb_droit_acces_page[] = $obj3->ID_page;
+				}
+			}
+
+			return $nb_droit_acces_page;
 		}
 
 		/**
