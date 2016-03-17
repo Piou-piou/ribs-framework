4fa2713d0ce7e7d1d3683a7318c0b70c0dc1aeb1
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-03-17 16:45:00 +0100

    update CheckVersoinModule to simplify it

--- core/modules/CheckVersionModule.php
+++ core/modules/CheckVersionModule.php
@@ -57,7 +57,6 @@
 		public function getCheckModuleVersion() {
 			$dbc = App::getDb();
 			$today = date("Y-m-d");
-			$today_o = new \DateTime($today);
 
 			$query = $dbc->query("SELECT next_check_version, version, url_telechargement, mettre_jour, delete_old_version, ID_module FROM module");
 
@@ -66,7 +65,7 @@
 					if ($obj->next_check_version == null) {
 						//si pas de version a checker, cela veut dire qu'on vient d'installer le module
 						//du coup on met le next_check aa la semaine pro
-						$set_next = true;
+						$this->setNextCheckUpdate($obj->ID_module);
 					}
 					else if (($obj->next_check_version <= $today) && ($obj->mettre_jour != 1)) {
 						//avant tout on regarde si on doit delete une vieille version
@@ -81,44 +80,7 @@
 
 						$version_txt = implode(".", $explode)."_version.txt";
 
-						if (file_get_contents($version_txt) !== "") {
-
-							//online pour bdd
-							$version_online_txt = file_get_contents($version_txt);
-
-							$version_online = floatval($version_online_txt);
-							$version_site = floatval($obj->version);
-
-							//la version sur le serveur de telechargement est plus récente, on va donc proposer
-							//en passant la valeur update a 1 dans la table module pour ce module
-							// au client de mettre a jour sa version sinon on met la next check a la semaine pro
-							if ($version_online > $version_site) {
-								$value = [
-									"update" => 1,
-									"online_version" => $version_online_txt,
-									"id_module" => $obj->ID_module
-								];
-
-								//on met la notification admin à 1
-								$dbc->query("UPDATE notification SET admin=1 WHERE ID_notification=1");
-
-								$dbc->prepare("UPDATE module SET mettre_jour=:update, online_version=:online_version WHERE ID_module=:id_module", $value);
-
-								$set_next = true;
-							}
-							else {
-								$set_next = true;
-							}
-						}
-					}
-
-					if ((isset($set_next)) && ($set_next === true)) {
-						$value = [
-							"next_check" => $today_o->add(new \DateInterval("P1W"))->format("Y-m-d"),
-							"id_module" => $obj->ID_module
-						];
-
-						$dbc->prepare("UPDATE module SET next_check_version=:next_check WHERE ID_module=:id_module", $value);
+						$this->getCheckModuleUpToDate($version_txt, $obj->version, $obj->ID_module);
 					}
 				}
 			}
@@ -131,5 +93,57 @@
 			$this->nom = $nom_module;
 			$this->version = $version;
 			$this->online_version = $online_version;
+		}
+
+		/**
+		 * @param $id_module
+		 * fonction qui permet de palnnifier la nouvelle check de si il il y aa une new maj du module
+		 */
+		private function setNextCheckUpdate($id_module) {
+			$dbc = App::getDb();
+			$today_o = new \DateTime(date("Y-m-d"));
+
+			$value = [
+				"next_check" => $today_o->add(new \DateInterval("P1W"))->format("Y-m-d"),
+				"id_module" => $id_module
+			];
+
+			$dbc->prepare("UPDATE module SET next_check_version=:next_check WHERE ID_module=:id_module", $value);
+		}
+
+		/**
+		 * @param $version_txt
+		 * @param $version
+		 * @param $id_module
+		 * fonction qui va vérifier si notre mdule est bien a jour
+		 */
+		private function getCheckModuleUpToDate($version_txt, $version, $id_module) {
+			$dbc = App::getDb();
+
+			if (file_get_contents($version_txt) !== "") {
+				//online pour bdd
+				$version_online_txt = file_get_contents($version_txt);
+
+				$version_online = floatval($version_online_txt);
+				$version_site = floatval($version);
+
+				//la version sur le serveur de telechargement est plus récente, on va donc proposer
+				//en passant la valeur update a 1 dans la table module pour ce module
+				// au client de mettre a jour sa version sinon on met la next check a la semaine pro
+				if ($version_online > $version_site) {
+					$value = [
+						"update" => 1,
+						"online_version" => $version_online_txt,
+						"id_module" => $id_module
+					];
+
+					//on met la notification admin à 1
+					$dbc->query("UPDATE notification SET admin=1 WHERE ID_notification=1");
+
+					$dbc->prepare("UPDATE module SET mettre_jour=:update, online_version=:online_version WHERE ID_module=:id_module", $value);
+				}
+			}
+
+			$this->setNextCheckUpdate($id_module);
 		}
 	}
