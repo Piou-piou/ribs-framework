99fb41337697df8ce4da6ffd180232279abb1d56
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-03-18 17:37:00 +0100

    update Navigation class

--- core/Navigation.php
+++ core/Navigation.php
@@ -12,7 +12,12 @@
 
 			if (is_array($query) && (count($query) > 0)) {
 				foreach ($query as $obj) {
-					$this->getLienNavigation($obj->ID_page, $obj->ID_module);
+					if ($obj->ID_page === null) {
+						$this->getLienNavigationModule($obj->ID_module);
+					}
+					else {
+						$this->getLienNavigationPage($obj->ID_page);
+					}
 				}
 			}
 		}
@@ -21,36 +26,36 @@
 		
 		
 		//-------------------------- GETTER ----------------------------------------------------------------------------//
-		private function getLienNavigation($id_page, $id_module) {
+		private function getLienNavigationPage($id_page) {
 			$dbc = App::getDb();
 
-			//pour un module
-			if ($id_page == "") {
-				$query = $dbc->select()
-					->from("navigation")
-					->from("module")
-					->where("navigation.ID_module", "=", "module.ID_module", "AND")
-					->where("module.ID_module", "=", $id_module)
-					->get();
+			$query = $dbc->select()
+				->from("navigation")
+				->from("page")
+				->where("navigation.ID_page", "=", "page.ID_page", "AND")
+				->where("page.ID_page", "=", $id_page)
+				->get();
 
-				if (is_array($query) && (count($query) > 0)) {
-					foreach ($query as $obj) {
-						echo($obj->nom_module." ++ ".$obj->url."<br>");
-					}
+			if (is_array($query) && (count($query) > 0)) {
+				foreach ($query as $obj) {
+					echo($obj->titre." ++ ".$obj->url."<br>");
 				}
 			}
-			else {
-				$query = $dbc->select()
-					->from("navigation")
-					->from("page")
-					->where("navigation.ID_page", "=", "page.ID_page", "AND")
-					->where("page.ID_page", "=", $id_page)
-					->get();
+		}
 
-				if (is_array($query) && (count($query) > 0)) {
-					foreach ($query as $obj) {
-						echo($obj->titre." ++ ".$obj->url."<br>");
-					}
+		private function getLienNavigationModule($id_module) {
+			$dbc = App::getDb();
+
+			$query = $dbc->select()
+				->from("navigation")
+				->from("module")
+				->where("navigation.ID_module", "=", "module.ID_module", "AND")
+				->where("module.ID_module", "=", $id_module)
+				->get();
+
+			if (is_array($query) && (count($query) > 0)) {
+				foreach ($query as $obj) {
+					echo($obj->nom_module." ++ ".$obj->url."<br>");
 				}
 			}
 		}
