783ff2dcd3fe1fcaeccc3859d7d3f353ca68afc7
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-03-31 17:04:24 +0200

    update initialie all in app

--- app/controller/initialise_all.php
+++ app/controller/initialise_all.php
@@ -1,7 +1,3 @@
 <?php
 	$gestion_module = new \core\modules\GestionModule();
 	$gestion_module->getListeModuleActiver();
-
-	if (\core\modules\GestionModule::getModuleActiver("galerie photo") == true) {
-		$gphoto = new \modules\galerie_photo\app\controller\GaleriePhoto(null, 1);
-	}
