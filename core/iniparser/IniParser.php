e28dea206a8de8abb76249a6bf3b731282c23ba3
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-06-13 17:19:36 +0200

    Adding SMTP configuraton in IniParser

--- core/iniparser/IniParser.php
+++ core/iniparser/IniParser.php
@@ -48,13 +48,23 @@
 				dev[DB_USER] = $dev_info[2]
 				dev[DB_PASS] = $dev_info[3]
 				dev[DB_HOST] = $dev_info[4]
+				dev[SMTP_HOST] =
+				dev[SMTP_USER] =
+				dev[SMTP_PASS] =
+				dev[SMTP_SECURE] =
+				dev[SMTP_PORT] =
 
 				[prod]
 				prod[DB_TYPE] = $prod_info[0]
 				prod[DB_NAME] = $prod_info[1]
 				prod[DB_USER] = $prod_info[2]
 				prod[DB_PASS] = $prod_info[3]
-				prod[DB_HOST] = $prod_info[4]";
+				prod[DB_HOST] = $prod_info[4]
+				prod[SMTP_HOST] =
+				prod[SMTP_USER] =
+				prod[SMTP_PASS] =
+				prod[SMTP_SECURE] =
+				prod[SMTP_PORT] =";
 
 			$value = str_replace("\t", "", $value);
 
