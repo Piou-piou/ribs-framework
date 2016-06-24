2d3848292fdfe4b23f482b88c85a8d685724544a
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-06-24 14:23:30 +0200

    Edit content update to see subnavigation in nav

--- admin/views/gestion-contenus/header.php
+++ admin/views/gestion-contenus/header.php
@@ -3,9 +3,9 @@
 		<ul>
 			<?php foreach (\core\App::getNav(1)->getNavigation() as $nav):?>
 				<li><a href="<?=ADMWEBROOT?>gestion-contenus/modifier-contenu?id=<?=$nav[0]?>" title="<?=$nav[3]?>"><?=$nav[1]?></a>
-					<?php if ((isset($nav[5]) && count($nav[5]) > 0)):?>
+					<?php if ((isset($nav[6]) && count($nav[6]) > 0)):?>
 						<ul>
-							<?php foreach ($nav[5] as $snav):?>
+							<?php foreach ($nav[6] as $snav):?>
 								<li><a href="<?=ADMWEBROOT?>gestion-contenus/modifier-contenu?id=<?=$snav[0]?>" title="<?=$snav[3]?>"><?=$snav[1]?></a></li>
 							<?php endforeach; ?>
 						</ul>
