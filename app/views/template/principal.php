015dd9a470a3a81bb3d5a8147be0ca802b0e1bfd
Anthony Pilloud <pilloud.anthony@gmail.com>
2016-06-27 16:11:53 +0200

    Moving navigation in a new file to add it where we want

--- app/views/template/principal.php
+++ app/views/template/principal.php
@@ -19,37 +19,9 @@
 	</head>
 	<?=\core\HTML\flashmessage\FlashMessage::getFlash();?>
 	<body>
-		<header>
-			<nav class="nav-no-respsonsive">
-				<div class="inner">
-					<ul>
-						<?php if ($config->getContenusDynamique() == 1):?>
-							<?php foreach (\core\App::getNav()->getNavigation() as $nav):?>
-								<li><a href="<?=WEBROOT.$nav[2]?>" title="<?=$nav[3]?>" target="<?=$nav[5]?>"><?=$nav[1]?></a>
-									<?php if ((isset($nav[6]) && count($nav[6]) > 0)):?>
-										<ul>
-											<?php foreach ($nav[6] as $snav):?>
-												<li><a href="<?=WEBROOT.$snav[2]?>" title="<?=$snav[3]?>"><?=$snav[1]?></a></li>
-											<?php endforeach;?>
-										</ul>
-									<?php endif;?>
-								</li>
-							<?php endforeach;?>
-						<?php endif;?>
+		<?php require_once(ROOT."app/views/template/navigation.php");?>
+		<?php if ($config->getResponsive() == 1) require_once(ROOT."app/views/template/nav_responsive.php");?>
 
-						<?php if ($config->getActiverConnexion() == 1):?>
-						<li><a href="">Connexion</a></li>
-						<?php endif;?>
-
-						<?php if ($config->getActiverInscription() == 1):?>
-							<li><a href="">Inscription</a></li>
-						<?php endif;?>
-					</ul>
-				</div>
-			</nav>
-
-			<?php if ($config->getResponsive() == 1) require_once(ROOT."app/views/template/nav_responsive.php");?>
-		</header>
 
 		<?php require($page.".php"); ?>
 
