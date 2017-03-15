7eb6a493c67acdab96cff3a344db03c79cbda4fe
Piou-piou <pilloud.anthony@gmail.com>
2017-03-15 12:02:55 +0100

    change logout to correctly use FlashMessage

--- core/auth/connexion/logout.php
+++ core/auth/connexion/logout.php
@@ -1,5 +1,7 @@
 <?php
 	namespace core\auth;
 	
+	use core\HTML\flashmessage\FlashMessage;
+	
 	FlashMessage::setFlash("Vous avez été déconnecté avec succès", "success");
 	Connexion::setDeconnexion(WEBROOT."index.php");
