076aeda2b04c0a1f1563af478a519c391acbf129
Piou-piou <pilloud.anthony@gmail.com>
2017-03-15 13:24:43 +0100

    edit admin version

--- admin/views/template/principal.php
+++ admin/views/template/principal.php
@@ -19,7 +19,7 @@
 	<body>
 		<nav class="menu <?php if (($_SESSION["menu_plie".CLEF_SITE] == "deplie") || (!isset($_SESSION["menu_plie".CLEF_SITE]))):?>active<?php endif;?>">
 			<div class="titre">
-				<h1>Ribs V2.3.5.6</h1>
+				<h1>Ribs V0.1</h1>
 				<i class="fa fa-bars"></i>
 			</div>
 
