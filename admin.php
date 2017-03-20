db301825423c6fbdcf22805c1972d03bb83e86df
Piou-piou <pilloud.anthony@gmail.com>
2017-03-20 12:56:04 +0100

    begin twig transition in administrator

--- admin.php
+++ admin.php
@@ -110,6 +110,13 @@
 				}
 				else {
 					$admin = new Admin($_SESSION["idlogin".CLEF_SITE]);
+					$twig_ok_page = ["contacter-support"];
+					
+					if (in_array($page, $twig_ok_page)) {
+						$loader = new Twig_Loader_Filesystem("admin/views/");
+						$twig = new Twig_Environment($loader);
+						$twig_page = true;
+					}
 					
 					require(ROOT."admin/controller/initialise_all.php");
 					require(ROOT."admin/views/template/principal.php");
