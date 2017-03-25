e0b5973e583572b3e59321f0c5a7b0a48adc6f55
Piou-piou <pilloud.anthony@gmail.com>
2017-03-25 23:49:36 +0100

    refactoring router of app to simplify it

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	//--------------------------------------------- INITIALISATION -------------------------------------------------------//
 	$page_root = "index.php";
 
 	require("vendor/autoload.php");
@@ -10,6 +11,12 @@
 	use \core\Configuration;
 
 	require("config/initialise.php");
+	
+	$page = "index";
+	if ((isset($_GET['page'])) && ($_GET['page'] != null)) {
+		$page = $_GET['page'];
+	}
+	//--------------------------------------------- FIN INITIALISATION DES CLASS -------------------------------------------------------//
 
 
 	//--------------------------------------------- INITIALISATION DES CLASS -------------------------------------------------------//
@@ -29,14 +36,8 @@
 	if ($config->getContenusDynamique() == 1) {
 		//initialisation des contenus
 		$contenu = new Contenus();
-
-		if ((isset($_GET['page'])) && ($_GET['page'] != null)) {
-			$page = $_GET['page'];
-			$contenu->getHeadPage(0, $page);
-		}
-		else {
-			$contenu->getHeadPage(1);
-		}
+		
+		$contenu->getHeadPage(0, $page);
 
 		\core\App::setTitle($contenu->getBaliseTitle());
 		\core\App::setDescription($contenu->getMetaDescription());
@@ -47,88 +48,67 @@
 
 
 	//--------------------------------------------- ROUTING -------------------------------------------------------//
-	if ((isset($_GET['page'])) && ($_GET['page'] != "")) {
-		$page = $_GET['page'];
-
-		$find = 'controller/';
-		$pos = strpos($page, $find);
-
-		if ($pos !== false) {
-			//recherche savoir si le fichier appele fait parti du core du systeme pour construire le lien
-			$find_core = 'controller/core/';
-			$core = strpos($page, $find_core);
-
-			//recherche savoir si le fichier appele est un module du systeme pour construire le lien
-			$find_module = 'controller/modules/';
-			$module = strpos($page, $find_module);
-
-			$explode = explode("/", $page, 2);
-			foreach ($explode as $lien);
-
-			//si c'est un controleur de base on va cerhcer dans core/admin
-			if ($core !== false) {
-				require_once(ROOT.$lien.".php");
-			}
-			else if ($module !== false) {
-				$explode = explode("/", $lien, 3);
-
-				require_once(ROOT.$explode[0]."/".$explode[1]."/app/controller/".$explode[2].".php");
-			}
-			else {
-				require_once("app/controller/".$lien.".php");
-			}
+	$find = 'controller/';
+	$pos = strpos($page, $find);
+	
+	if ($pos !== false) {
+		//recherche savoir si le fichier appele fait parti du core du systeme pour construire le lien
+		$find_core = 'controller/core/';
+		$core = strpos($page, $find_core);
+		
+		//recherche savoir si le fichier appele est un module du systeme pour construire le lien
+		$find_module = 'controller/modules/';
+		$module = strpos($page, $find_module);
+		
+		$explode = explode("/", $page, 2);
+		foreach ($explode as $lien);
+		
+		//si c'est un controleur de base on va cerhcer dans core/admin
+		if ($core !== false) {
+			require_once(ROOT.$lien.".php");
+		}
+		else if ($module !== false) {
+			$explode = explode("/", $lien, 3);
+			
+			require_once(ROOT.$explode[0]."/".$explode[1]."/app/controller/".$explode[2].".php");
 		}
 		else {
-			$cache = new \core\Cache($page);
-
-			$router_module = new RouterModule();
-			if ($router_module->getRouteModuleExist($page)) {
-				$page = $router_module->getUrl($page);
-
-				if ($router_module->getController() != "") {
-					require_once(MODULEROOT.$router_module->getController());
-				}
-
-				$loader = new Twig_Loader_Filesystem(['modules/'.$router_module->getModule()."/app/views", "app/views"]);
-				$twig = new Twig_Environment($loader);
-				$arr_page = \core\App::getValues();
-				$page = $router_module->getPage();
-			}
-			else {
-				$contenu->getContenuPage();
-				$contenu_page = $contenu->getContenu();
-
-				$explode = explode("/", $page);
-
-				$loader = new Twig_Loader_Filesystem('app/views');
-				$twig = new Twig_Environment($loader);
-				
-				$arr_page = array_merge(core\App::getValues(), ["contenu_page" => $contenu_page]);
-				$page = end($explode);
-
-				if (!file_exists(ROOT."app/views/".$page.".html")) {
-					\core\RedirectError::Redirect(404);
-				}
-			}
-			
-			if ($cache->setStart() === false) {
-				require("app/controller/initialise_all.php");
-				require("app/views/template/principal.php");
-			}
-			$cache->setEnd();
+			require_once("app/controller/".$lien.".php");
 		}
 	}
 	else {
-		$contenu->getContenuPage();
-		$contenu_page = $contenu->getContenu();
-
-		$loader = new Twig_Loader_Filesystem('app/views');
-		$twig = new Twig_Environment($loader);
-
-		$arr_page = array_merge(core\App::getValues(), ["contenu_page" => $contenu_page]);
-		$page = "index";
-		
 		$cache = new \core\Cache($page);
+		
+		$router_module = new RouterModule();
+		if ($router_module->getRouteModuleExist($page)) {
+			$page = $router_module->getUrl($page);
+			
+			if ($router_module->getController() != "") {
+				require_once(MODULEROOT.$router_module->getController());
+			}
+			
+			$loader = new Twig_Loader_Filesystem(['modules/'.$router_module->getModule()."/app/views", "app/views"]);
+			$twig = new Twig_Environment($loader);
+			$arr_page = \core\App::getValues();
+			$page = $router_module->getPage();
+		}
+		else {
+			$contenu->getContenuPage();
+			$contenu_page = $contenu->getContenu();
+			
+			$explode = explode("/", $page);
+			
+			$loader = new Twig_Loader_Filesystem('app/views');
+			$twig = new Twig_Environment($loader);
+			
+			$arr_page = array_merge(core\App::getValues(), ["contenu_page" => $contenu_page]);
+			$page = end($explode);
+			
+			if (!file_exists(ROOT."app/views/".$page.".html")) {
+				\core\RedirectError::Redirect(404);
+			}
+		}
+		
 		if ($cache->setStart() === false) {
 			require("app/controller/initialise_all.php");
 			require("app/views/template/principal.php");
