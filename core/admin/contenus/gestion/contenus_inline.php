ec6ec3949fe3ed7e16d62018d4b91136de79ae77
Piou-piou <pilloud.anthony@gmail.com>
2017-03-28 13:08:01 +0200

    update inline edition to use new right access system

--- core/admin/contenus/gestion/contenus_inline.php
+++ core/admin/contenus/gestion/contenus_inline.php
@@ -1,17 +1,16 @@
 <?php
-	$gestion_contenu = new \core\admin\contenus\GestionContenus(0, 1);
+	$gestion_contenu = new \core\admin\contenus\GestionContenus($_POST['url']);
 	$droit_acces = new \core\admin\droitsacces\DroitAcces();
-
-	if ((($droit_acces->getDroitAccesContenu("GESTION CONTENU PAGE", $_POST['id_page']) == true) &&  ($droit_acces->getModifContenu() != 0)) || ($droit_acces->getSuperAdmin() == 1)) {
-
-		$gestion_contenu->setModifierContenu($_POST['id_page'], $_POST['contenu']);
-
-		if ((\core\App::getErreur() !== true) && ($gestion_contenu->getErreur() !== true)) {
+	
+	if ($droit_acces->getSuperAdmin() == 1 || in_array("GESTION CONTENUS", $droit_acces->getListeDroitsAcces())) {
+		$droit_acces->getListeDroitModificationContenu($_POST['id_page']);
+		
+		if ($droit_acces->getModifContenu() != 0 || $droit_acces->getSuperAdmin() == 1) {
+			$gestion_contenu->setModifierContenu($_POST['id_page'], $_POST['contenu']);
+			
 			\core\HTML\flashmessage\FlashMessage::setFlash("La page ".$_POST['titre_page']." a été mise à jour", "success");
 		}
 	}
 	else {
 		\core\HTML\flashmessage\FlashMessage::setFlash("Vous n'avez pas l'autorisation de modifier cette page !");
 	}
-
-	header("location:".ADMWEBROOT."gestion-contenus/inline?id=".$_POST['id_page']);
