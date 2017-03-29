517a2a88f93794046c8e431bbf1b7443417de5a3
Piou-piou <pilloud.anthony@gmail.com>
2017-03-29 19:27:35 +0200

    refactoring Encrypt

--- core/auth/Encrypt.php
+++ core/auth/Encrypt.php
@@ -7,7 +7,6 @@
 	class Encrypt {
 		private static $params;
 		
-		
 		//-------------------------- CONSTRUCTEUR ----------------------------------------------------------------------------//
 		//-------------------------- FIN CONSTRUCTEUR ----------------------------------------------------------------------------//
 		
@@ -47,36 +46,21 @@
 		 */
 		public static function setEncryptMdp($mdp, $id_identite = null) {
 			$encrypt_str = ChaineCaractere::random(154);
-
-			//on cache le mdp
 			$mdp = md5($mdp);
-
-			//récupération de la logneur du mot de passe
 			$longeur_mdp = strlen($mdp);
-
-			//on va couper le mot de passe en 2 suivant un nombre aleatoire
 			$nb_aleatoire_mdp = rand(3, $longeur_mdp-2);
 			$bout1_mdp = mb_substr($mdp, 0, $longeur_mdp/2, "UTF-8");
 			$bout2_mdp = mb_substr($mdp, $longeur_mdp/2, $longeur_mdp, "UTF-8");
-
-			//on stock la taille des bouts pour pouvoir les décrypter
 			$taille_bout1 = strlen($bout1_mdp);
 			$taille_bout2 = strlen($bout2_mdp);
-
-			//on insere le premier bout aleatoirement dans le hashmdp
-			//on calcul sa longeur total (celle duhash + la logneur du mdp que l'on va rajouter dedans)
 			$longueur_hash = strlen($encrypt_str);
 			$debut_bout1 = rand(0, $longueur_hash/2);
-
-			//on rajouter le premier bout dans le mot de passe + recalcule de la longeur du hash avec le mdp add
+			
 			$encrypt_str = mb_substr($encrypt_str, 0, $debut_bout1).$bout1_mdp.mb_substr($encrypt_str, $debut_bout1, $longueur_hash);
-
-			//on insere le second bout aleatoirement dans le hashmdp
-			//on calcul sa longeur total (celle duhash + la logneur premier bout du mdp que l'on va rajouter dedans)
+			
 			$longueur_hash = strlen($encrypt_str);
 			$debut_bout2 = rand($longueur_hash/2, $longueur_hash);
-
-			//on rajoute le deuxieme
+			
 			$mdp_encrypt = mb_substr($encrypt_str, 0, $debut_bout2).$bout2_mdp.mb_substr($encrypt_str, $debut_bout2, $longueur_hash);
 
 			if ($id_identite != null) {
@@ -85,7 +69,6 @@
 			else {
 				self::$params = "$taille_bout1, $debut_bout2, $nb_aleatoire_mdp, $taille_bout2, $debut_bout1, ".ChaineCaractere::random(20);
 			}
-
 			return $mdp_encrypt;
 		}
 
