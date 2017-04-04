7bcf4c93e4a7cbd4fb5f90c324eaea8ac266b624
Piou-piou <pilloud.anthony@gmail.com>
2017-04-04 16:25:29 +0200

    update now foundation is used in cdn

--- app/views/template/principal.php
+++ app/views/template/principal.php
@@ -4,12 +4,12 @@
 		<title><?=$arr_page["app"]["contenus"]["balise_title"]?></title>
 		<meta charset="utf-8">
 		<meta name="description" content="<?=$arr_page["app"]["contenus"]["meta_description"]?>">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-		<link rel="stylesheet" type="text/css" href="<?=TPLWEBROOT?>css/foundation.css">
-		<script src="<?=TPLWEBROOT?>js/nav-responsive.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css">
 		<link rel="stylesheet" type="text/css" href="<?=TPLWEBROOT?>css/style.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script src="<?=TPLWEBROOT?>js/nav-responsive.js"></script>
 	</head>
 	<?=\core\HTML\flashmessage\FlashMessage::getFlash();?>
 	<body>
